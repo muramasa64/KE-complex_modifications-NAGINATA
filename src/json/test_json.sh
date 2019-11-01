c450df7317ac9d8bdf48c014681f043a3e302656
ISOBE Kazuhiko <muramasa64@gmail.com>
2019-11-01 18:10:48 +0900

    Dvorak配列のテストを追加

--- src/json/test_json.sh
+++ src/json/test_json.sh
@@ -24,3 +24,23 @@ rm -f qwerty_h_m.json
 ruby ./japanese_naginata.json.rb v m > qwerty_v_m.json
 diff -u qwerty_v_m.json ../../docs/json/japanese_naginata_m.json
 rm -f qwerty_v_m.json
+
+# Dvorak, Horizonal
+ruby ./dvorak_japanese_naginata.json.rb dh > dvorak_h.json
+diff -u dvorak_h.json ../../docs/json/dvorak_japanese_naginata_h.json
+rm -f drovak_h.json
+
+# Drovak, Vertical
+ruby ./dvorak_japanese_naginata.json.rb dv > dvorak_v.json
+diff -u drovak_v.json ../../docs/json/dvorak_japanese_naginata.json
+rm -f drovak_v.json
+
+# Dvorak, Horizonal, T/Y key Exchange
+ruby ./dvorak_japanese_naginata.json.rb dh m > drovak_h_m.json
+diff -u drovak_h_m.json ../../docs/json/dvorak_japanese_naginata_h_m.json
+rm -f drovak_h_m.json
+
+# Dvorak, Vertical, T/Y Key Exchange
+ruby ./dvorak_japanese_naginata.json.rb dv m > drovak_v_m.json
+diff -u drovak_v_m.json ../../docs/json/dvorak_naginata_m.json
+rm -f drovak_v_m.json
